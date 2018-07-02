ef07ed340b813e0cecdc0038ffc805d2b2fb3659
aslam <aslam.a@osmosys.asia>
2018-07-02 19:13:53 +0530

    Added app center permissons

---
+++ Pods/Target Support Files/Pods-ci-cd-test/Pods-ci-cd-test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ci_cd_test : NSObject
+@end
+@implementation PodsDummy_Pods_ci_cd_test
+@end
--- ci-cd-test/AppDelegate.swift
+++ ci-cd-test/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("1ac97148-57d0-41ff-afb3-331886f7334f", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
